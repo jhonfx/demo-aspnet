0fef699895f7b039a5c264c9d941c3137615aacb
Juan Francisco Purata  Fernandez <jpurataf@EKT1034455MAC.local>
2019-03-25 21:00:47 -0600

    first commit files

---
+++ Controllers/EscuelaController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using HolaMundoMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HolaMundoMVC.Controllers
+{
+    public class EscuelaController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            var escuela = new Escuela();
+            escuela.escuelaId = "13567";
+            escuela.nombreEscuela = "Vasconcelos";
+            escuela.anioFundacion = Guid.NewGuid().ToString();
+            escuela.TipoEscuela = TiposEscuela.Secundaria;
+
+            var alumnos = new List<String>();
+            alumnos.Add("Juan");
+            alumnos.Add("Maleni");
+            alumnos.Add("Ximena");
+            alumnos.Add("Renata");
+
+            ViewBag.cosaDinamica = alumnos;
+            return View(escuela);
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+        
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        }
+    }
+}
---
+++ Models/Escuela.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace HolaMundoMVC.Models
+{
+    public class Escuela : ObjetoEscuelaBase
+    {
+        public string escuelaId { get; set; }
+        public string nombreEscuela { get; set; }
+        public string anioFundacion { get; set; }
+        public string pais { get; set; }
+        public string ciudad { get; set; }
+
+        public TiposEscuela TipoEscuela { get; set; }
+
+        public void Configure(IApplicationBuilder app)
+        {
+           
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        }
+
+        public override string ToString()
+        {
+            return $"Nombre: \"{Nombre}\", Tipo: {TipoEscuela} {System.Environment.NewLine} Pais: {pais}, Ciudad: {ciudad}";
+        }
+    }
+}
